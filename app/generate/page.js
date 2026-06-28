27c0bf2bf1c33e2beca5b47a5cd81edf1353f376
websitedeveloper997 <websitedeveloper997@gmail.com>
2026-06-28 18:13:55 +0500

    Update page.js

--- app/generate/page.js
+++ app/generate/page.js
@@ -8,7 +8,7 @@ export const dynamic = "force-dynamic";
 
 const Generate = () => {
   const searchParams = useSearchParams();
-  const router = useRouter(); // ✅ ADD THIS
+  const router = useRouter(); 
 
   const [links, setLinks] = useState([{ link: "", linktext: "" }]);
   const [handle, sethandle] = useState(searchParams.get("handle") || "");
