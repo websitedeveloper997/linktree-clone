7ca38506329fa890ce4ba9dc843671970b0deb53
websitedeveloper997 <websitedeveloper997@gmail.com>
2026-06-29 18:12:49 +0500

    Update page.js

--- app/[handle]/page.js
+++ app/[handle]/page.js
@@ -13,8 +13,7 @@ export default async function Page({ params }) {
   const { handle } = params;
 
   try {
-    const baseUrl =
-      process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
     const res = await fetch(
       `${baseUrl}/api/get?handle=${handle}`,
