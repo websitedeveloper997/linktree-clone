ca6f943264d88ed335f8a1fb9ed5a76b73c3557d
websitedeveloper997 <websitedeveloper997@gmail.com>
2026-06-30 15:01:26 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -46,7 +46,7 @@ export default function Home() {
               <input
                 value={text}
                 onChange={(e) => setText(e.target.value)}
-                className="px-4 py-3 rounded-md bg-gray-900 text-white border border-gray-700 focus:outline-none focus:border-white w-full sm:w-auto"
+                className="px-4 py-3 rounded-md bg-[#262626] text-white border border-white/20 focus:outline-none focus:border-white w-full sm:w-auto"
                 type="text"
                 placeholder="Enter your Handle"
               />
